cf4a5b0a370238390a25ed6a6572e2ed3f211046
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2022-12-31 21:13:04 +0900

    Create solution.
    - Create Console project.
    - Use System.CommandLine.
    - Add EditorConfig.

---
+++ Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+
+var fileInfoArray = new Argument<FileInfo[]>();
+
+var rootCommand = new RootCommand();
+rootCommand.AddArgument(fileInfoArray);
+
+rootCommand.SetHandler(fileInfoArray =>
+{
+    foreach (var fileInfo in fileInfoArray)
+        Console.WriteLine($"Hello, World! {fileInfo.Name}");
+}, fileInfoArray);
+
+await rootCommand.InvokeAsync(args);
